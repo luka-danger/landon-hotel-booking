50e578f235303312cedb27311f78949baf9c32cd
luka-danger <lukathevegan@gmail.com>
2024-04-18 12:13:37 -0600

    Add message executor to call multithreads

--- src/main/java/edu/wgu/d387_sample_code/il8n/WelcomeMessage.java
+++ src/main/java/edu/wgu/d387_sample_code/il8n/WelcomeMessage.java
@@ -4,17 +4,41 @@ import org.springframework.core.io.ClassPathResource;
 
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class WelcomeMessage {
+
+    static ExecutorService messageExecutor = Executors.newFixedThreadPool(2);
+
     public static void main(String[] args) {
+        String welcomeMessage = getWelcomeMessage();
+        System.out.println(welcomeMessage);
+    }
+
+    public static String getWelcomeMessage() {
         Properties properties = new Properties();
-        try {
-            InputStream stream = new ClassPathResource("welcome_fr_CA.properties").getInputStream();
-            properties.load(stream);
-            System.out.println(properties.getProperty("welcome"));
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+        messageExecutor.execute(() -> {
+            try {
+                InputStream streamFrench = new ClassPathResource("welcome_fr_CA.properties").getInputStream();
+                properties.load(streamFrench);
+                System.out.println(properties.getProperty("welcome"));
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        messageExecutor.execute(() -> {
+            try {
+                InputStream streamEnglish = new ClassPathResource("welcome_en_US.properties").getInputStream();
+                properties.load(streamEnglish);
+                System.out.println(properties.getProperty("welcome"));
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("welcome");
+            }
+        });
+        return properties.getProperty("welcome");
     }
 }
